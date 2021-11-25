5830e8dffe4acd4e60cc0f392c0591d40484ef62
will <willnorcliffebrown@gmail.com>
2021-11-25 16:58:17 +0000

    move creating of arrays further down for ease of parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use numpy::ndarray::{Dim};
-use numpy::{IntoPyArray, PyArray};
+use numpy::PyArray;
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 use::pyo3::PyErr;
 use::pyo3::exceptions::{PyValueError, PyIOError};
@@ -7,33 +7,23 @@ use serde_json::Value;
 
 mod parsing;
 use parsing::{parse_float_column, parse_keys};
-use parsing::{VectorTypes};
+use parsing::{NumpyTypes};
 
 
 
 
 #[pymodule]
 fn serde_numpy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-
-
-    fn _parse_float_array<'py>(py: Python<'py>, value: &Value, key: &str, index: usize) -> PyResult<&'py PyArray<f64, Dim<[usize; 1]>>> {
-        if let Ok(VectorTypes::FloatArray(vector)) = parse_float_column(&value, key, index) {
-            Ok(vector.into_pyarray(py))
-        }
-        else {
-            Err(PyErr::new::<PyValueError, _>("JSON Key does not exist!"))
-        }
-    }
     
     
     #[pyfn(m)]
     #[pyo3(name = "parse_float_array")]
-    fn parse_float_array<'py>(py: Python<'py>, json_str: &str, key: &str, index: usize) -> PyResult<&'py PyArray<f64, Dim<[usize; 1]>>> {
+    fn parse_float_array<'py>(py: Python<'py>, json_str: &str, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
 
         let result = serde_json::from_str(json_str);
         
         match result {
-            Ok(value) => _parse_float_array(py, &value, key, index),
+            Ok(value) => parse_float_column(py, &value, key, index),
             Err(_err) => return Err(PyErr::new::<PyIOError, _>("Invalid JSON"))
         }
 
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,19 +1,36 @@
 use std::collections::HashMap;
-use pyo3::types::IntoPyDict;
-use pyo3::{IntoPy, ToPyObject};
+
+use numpy::IntoPyArray;
+use numpy::PyArray;
+use numpy::ndarray::Dim;
+
+use pyo3::exceptions::{PyValueError, PyIOError};
+use pyo3::prelude::{Python, PyErr, PyResult, IntoPy, PyObject,};
+use pyo3::conversion::AsPyPointer;
 use serde_json::Value;
 
 
 #[derive(Debug)]
-pub enum VectorTypes {
-    IntArray(Vec<i64>),
-    FloatArray(Vec<f64>),
-    BoolArray(Vec<bool>),
-    StringArray(Vec<String>),
+pub enum NumpyTypes <'py> {
+    IntArray(&'py PyArray<i64, Dim<[usize; 1]>>),
+    FloatArray(&'py PyArray<f64, Dim<[usize; 1]>>),
+    BoolArray(&'py PyArray<bool, Dim<[usize; 1]>>),
+    // StringArray(&'py PyArray<String, Dim<[usize; 1]>>),
+}
+
+impl IntoPy<PyObject> for NumpyTypes<'_> {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        match self {
+            NumpyTypes::IntArray(array) => unsafe { PyObject::from_borrowed_ptr(py, array.as_ptr())},
+            NumpyTypes::FloatArray(array) => unsafe { PyObject::from_borrowed_ptr(py, array.as_ptr())},
+            NumpyTypes::BoolArray(array) => unsafe { PyObject::from_borrowed_ptr(py, array.as_ptr())},
+        }
+    }
 }
 
 
-pub fn parse_int_column(value: &Value, key: &str, index: usize) -> Result<VectorTypes, String> {
+
+pub fn parse_int_column<'py>(py: Python<'py>, value: &Value, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
     if let Some(stream) = value[key].as_array() {
         let mut out: Vec<i64> = vec![0; stream.len()];
         for i in 0..stream.len() {
@@ -21,15 +38,15 @@ pub fn parse_int_column(value: &Value, key: &str, index: usize) -> Result<Vector
                 out[i] = v;
             };
         }
-        Ok(VectorTypes::IntArray(out))
+        Ok(NumpyTypes::IntArray(out.into_pyarray(py)))
     }
     else {
-        Err(format!("json key {} does not exist", &key))
+        Err(PyErr::new::<PyValueError, _>(format!("json key {} does not exist", &key)))
     }
 }
 
 
-pub fn parse_float_column(value: &Value, key: &str, index: usize) -> Result<VectorTypes, String> {
+pub fn parse_float_column<'py>(py: Python<'py>, value: &Value, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
     if let Some(stream) = value[key].as_array() {
         let mut out: Vec<f64> = vec![0.0; stream.len()];
         for i in 0..stream.len() {
@@ -37,15 +54,15 @@ pub fn parse_float_column(value: &Value, key: &str, index: usize) -> Result<Vect
                 out[i] = v;
             };
         }
-        Ok(VectorTypes::FloatArray(out))
+        Ok(NumpyTypes::FloatArray(out.into_pyarray(py)))
     }
     else {
-        Err(format!("json key {} does not exist", &key))
+        Err(PyErr::new::<PyValueError, _>(format!("json key {} does not exist", &key)))
     }
 }
 
 
-pub fn parse_bool_column(value: &Value, key: &str, index: usize) -> Result<VectorTypes, String> {
+pub fn parse_bool_column<'py>(py: Python<'py>, value: &Value, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
     if let Some(stream) = value[key].as_array() {
         let mut out: Vec<bool> = vec![false; stream.len()];
         for i in 0..stream.len() {
@@ -53,90 +70,85 @@ pub fn parse_bool_column(value: &Value, key: &str, index: usize) -> Result<Vecto
                 out[i] = v;
             };
         }
-        Ok(VectorTypes::BoolArray(out))
+        Ok(NumpyTypes::BoolArray(out.into_pyarray(py)))
     }
     else {
-        Err(format!("json key {} does not exist", &key))
+        Err(PyErr::new::<PyValueError, _>(format!("json key {} does not exist", &key)))
     }
 }
 
 
-pub fn parse_str_column(value: &Value, key: &str, index: usize) -> Result<VectorTypes, String> {
-    if let Some(stream) = value[key].as_array() {
-        let mut out: Vec<String> = vec!["".to_string(); stream.len()];
-        for i in 0..stream.len() {
-            if let Some(v) = value[key][i][index].as_str() {
-                out[i] = v.to_string();
-            };
-        }
-        Ok(VectorTypes::StringArray(out))
-    }
-    else {
-        Err(format!("json key {} does not exist", &key))
-    }
-}
+// pub fn parse_str_column<'py>(py: Python<'py>, value: &Value, key: &str, index: usize) -> Result<NumpyTypes<'py>, String> {
+//     if let Some(stream) = value[key].as_array() {
+//         let mut out: Vec<String> = vec!["".to_string(); stream.len()];
+//         for i in 0..stream.len() {
+//             if let Some(v) = value[key][i][index].as_str() {
+//                 out[i] = v.to_string();
+//             };
+//         }
+//         Ok(NumpyTypes::StringArray(out.into_pyarray(py)))
+//     }
+//     else {
+//         Err(format!("json key {} does not exist", &key))
+//     }
+// }
 
 
-pub fn parse_column(value: &Value, key: &str, index: usize) -> Result<VectorTypes, String> {
+pub fn parse_column<'py>(py: Python<'py>, value: &Value, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
     let initial_value = &value[key][0][index];
     if initial_value.is_f64() {
-        return parse_float_column(value, key, index)
+        return parse_float_column(py, value, key, index)
     }
     else if initial_value.is_i64() {
-        return parse_int_column(value, key, index)
+        return parse_int_column(py, value, key, index)
     }
     else if initial_value.is_boolean() {
-        return parse_bool_column(value, key, index)
+        return parse_bool_column(py, value, key, index)
     }
-    else if initial_value.is_string() {
-        return parse_str_column(value, key, index)
-    }
-    Err(format!("cannot parse column with initial type {:?}", initial_value))
+    // else if initial_value.is_string() {
+    //     return parse_str_column(py, value, key, index)
+    // }
+    Err(PyErr::new::<PyIOError, _>(format!("cannot parse column with initial value {:?}", initial_value)))
 }
 
 
-pub fn parse_columns(value: &Value, key: &str, indexes: Vec<usize>) -> Result<Vec<VectorTypes>, String> {
+pub fn parse_columns<'py>(py: Python<'py>, value: &Value, key: &str, indexes: Vec<usize>) -> PyResult<Vec<NumpyTypes<'py>>> {
     let mut out = Vec::new();
     for index in indexes {
-        if let Ok(vector) = parse_column(&value, &key, index) {
+        if let Ok(vector) = parse_column(py, &value, &key, index) {
             out.push(vector);
         }
         else {
-            return Err(format!("cannot parse column at index {} for key {}", index, key))
+            return Err(PyErr::new::<PyIOError, _>(format!("cannot parse column at index {} for key {}", index, key)))
         }
     }
     Ok(out)
 }
 
 
-pub struct ArrayMap {
-    data: HashMap<String, Vec<VectorTypes>>
-}
+// pub struct ArrayMap {
+//     data: HashMap<String, Vec<NumpyTypes>>
+// }
 
-impl ArrayMap {
-    fn new() -> ArrayMap {
-        ArrayMap{data: HashMap::<String, Vec<VectorTypes>>::new()}
-    }
+// impl ArrayMap {
+//     fn new() -> ArrayMap {
+//         ArrayMap{data: HashMap::<String, Vec<NumpyTypes>>::new()}
+//     }
 
-}
+// }
 
-impl ArrayMap {
-    fn insert(&mut self, key: String, value: Vec<VectorTypes>) {
-        self.data.insert(key, value);
-    }
-}
-
-impl IntoPy for ArrayMap {
-
-}
+// impl ArrayMap {
+//     fn insert(&mut self, key: String, value: Vec<VectorTypes>) {
+//         self.data.insert(key, value);
+//     }
+// }
 
 
-
-pub fn parse_keys<'a>(value: &Value, keys: Vec<&'a str>, indexes: Vec<Vec<usize>>) -> Result<ArrayMap, String> {
+pub fn parse_keys<'py>(py: Python<'py>, value: &Value, keys: Vec<&str>, indexes: Vec<Vec<usize>>) -> PyResult<HashMap<String, Vec<NumpyTypes<'py>>>> {
     assert_eq!(keys.len(), indexes.len(), "Number of keys: {} is not the same as number of indexes: {}", keys.len(), indexes.len());
-    let mut out = ArrayMap::new();
+    let mut out = HashMap::new();
     for (&key, indexes_) in keys.iter().zip(indexes) {
-        let result = parse_columns(&value, key, indexes_);
+        let result = parse_columns(py, &value, key, indexes_);
         match result  {
             Ok(vector) => {out.insert(String::from(key), vector);},
             Err(err) => return Err(err)
