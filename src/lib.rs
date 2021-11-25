4fbbff55724c9ccf940e99837fbbdb98c7b0518b
will <willnorcliffebrown@gmail.com>
2021-11-25 17:22:33 +0000

    update tests and library functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use numpy::ndarray::{Dim};
 use numpy::PyArray;
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
@@ -6,7 +8,7 @@ use::pyo3::exceptions::{PyValueError, PyIOError};
 use serde_json::Value;
 
 mod parsing;
-use parsing::{parse_float_column, parse_keys};
+use parsing::{parse_float_column, parse_bool_column, parse_int_column, parse_keys};
 use parsing::{NumpyTypes};
 
 
@@ -14,8 +16,7 @@ use parsing::{NumpyTypes};
 
 #[pymodule]
 fn serde_numpy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    
-    
+        
     #[pyfn(m)]
     #[pyo3(name = "parse_float_array")]
     fn parse_float_array<'py>(py: Python<'py>, json_str: &str, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
@@ -24,6 +25,50 @@ fn serde_numpy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         
         match result {
             Ok(value) => parse_float_column(py, &value, key, index),
+            Err(_err) => return Err(PyErr::new::<PyIOError, _>("Invalid JSON"))
+        }
+
+        
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "parse_bool_array")]
+    fn parse_bool_array<'py>(py: Python<'py>, json_str: &str, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
+
+        let result = serde_json::from_str(json_str);
+        
+        match result {
+            Ok(value) => parse_bool_column(py, &value, key, index),
+            Err(_err) => return Err(PyErr::new::<PyIOError, _>("Invalid JSON"))
+        }
+
+        
+    }
+
+
+    #[pyfn(m)]
+    #[pyo3(name = "parse_int_array")]
+    fn parse_int_array<'py>(py: Python<'py>, json_str: &str, key: &str, index: usize) -> PyResult<NumpyTypes<'py>> {
+
+        let result = serde_json::from_str(json_str);
+        
+        match result {
+            Ok(value) => parse_int_column(py, &value, key, index),
+            Err(_err) => return Err(PyErr::new::<PyIOError, _>("Invalid JSON"))
+        }
+
+        
+    }
+
+
+    #[pyfn(m)]
+    #[pyo3(name = "parse_keys")]
+    fn parse_keys_<'py>(py: Python<'py>, json_str: &str, keys: Vec<&str>, indexes: Vec<Vec<usize>>) -> PyResult<HashMap<std::string::String, Vec<NumpyTypes<'py>>>> {
+
+        let result = serde_json::from_str(json_str);
+        
+        match result {
+            Ok(value) => parse_keys(py, &value, keys, indexes),
             Err(_err) => return Err(PyErr::new::<PyIOError, _>("Invalid JSON"))
         }
 
--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,51 @@
 import numpy as np
-from serde_numpy import parse_float_array
+from serde_numpy import parse_float_array, parse_int_array, parse_bool_array, parse_keys
+
+
+data = """
+    {"id": "161fjkjnbf", 
+    "stream_1": 
+    [[36, 60, 1610034058843.0, 1, 1, -519151558, 49, "", -1, false, false, false, true, 0, "Digit", "1", false, "-1", 20912.165, -1], 
+    [36, 61, 1610034058847.0, 2, 1, -519151558, 49, "1", -1, false, false, true, true, 0, "Digit", "1", false, "-1", 20918.465, 0], 
+    [36, 63, 1610034058888.0, 0, 1, -519151558, 49, "", -1, false, true, false, true, 0, "Digit", "1", false, "-1", 20986.94, -1]], 
+    "stream_2": 
+    [[36, 12, 1610034053955.0, 0, 1, 0, 1016, 746, 1016, 817, 0, 0, 1016, 746, 0, 0, 16049.98], 
+    [36, 13, 1610034053972.0, 0, 1, 0, 1020, 717, 1020, 788, 0, 0, 1020, 717, 0, 0, 16069.06], 
+    [36, 14, 1610034053989.0, 0, 1, 0, 1023, 688, 1023, 759, 0, 0, 1023, 688, 0, 0, 16083.675]]}"""
 
 
 def test_float():
-    data = """
-        {"id": "161fjkjnbf", 
-        "stream_1": 
-        [[36, 60, 1610034058843.0, 1, 1, -519151558, 49, "", -1, false, false, false, false, 0, "Digit", "1", false, "-1", 20912.165, -1], 
-        [36, 61, 1610034058847.0, 2, 1, -519151558, 49, "1", -1, false, false, false, false, 0, "Digit", "1", false, "-1", 20918.465, 0], 
-        [36, 63, 1610034058888.0, 0, 1, -519151558, 49, "", -1, false, false, false, false, 0, "Digit", "1", false, "-1", 20986.94, -1]], 
-        "stream_2": 
-        [[36, 12, 1610034053955.0, 0, 1, 0, 1016, 746, 1016, 817, 0, 0, 1016, 746, 0, 0, 16049.98], 
-        [36, 13, 1610034053972.0, 0, 1, 0, 1020, 717, 1020, 788, 0, 0, 1020, 717, 0, 0, 16069.06], 
-        [36, 14, 1610034053989.0, 0, 1, 0, 1023, 688, 1023, 759, 0, 0, 1023, 688, 0, 0, 16083.675]]}"""
     out = parse_float_array(data, "stream_1", 2)
+    assert out.dtype == np.float64
     assert np.array_equal(out, [1610034058843.0, 1610034058847.0, 1610034058888.0])
     assert isinstance(out, np.ndarray)
 
+
+def test_int():
+    out = parse_int_array(data, "stream_2", 1)
+    assert out.dtype == np.int64
+    assert np.array_equal(out, [12, 13, 14])
+    assert isinstance(out, np.ndarray)
+
+
+def test_bool():
+    out = parse_bool_array(data, "stream_1", 11)
+    assert out.dtype == np.bool
+    assert np.array_equal(out, [False, True, False])
+    assert isinstance(out, np.ndarray)
+
+def test():
+    out = parse_keys(data, ["stream_1", "stream_2"], [[0,1,2], [0,2,4]])
+    expected = {
+        'stream_2': [
+            np.array([36, 36, 36]), 
+            np.array([1610034053955.0, 1610034053972.0, 1610034053989.0]), 
+            np.array([1, 1, 1])], 
+        'stream_1': [
+            np.array([36, 36, 36]), 
+            np.array([60, 61, 63]), 
+            np.array([1610034058843.0, 1610034058847.0, 1610034058888.0])]
+            }
+    for k in out:
+        for i in range(len(out[k])):
+            assert np.array_equal(out[k][i], expected[k][i])
