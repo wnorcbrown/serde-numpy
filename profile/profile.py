8c8d48476ea65b086a11f5f3ea90768791be340e
will norcliffe <will.norcliffe@biocatch.com>
2022-07-14 17:12:16 +0100

    upgrade profiling script

--- profile/profile.py
+++ profile/profile.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Mapping, Sequence, Type, Tuple
+from typing import Callable, List, Mapping, Sequence, Type, Tuple, Any
 import json
 import time
 
@@ -6,33 +6,58 @@ import numpy as np
 import orjson
 import json
 import string
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 from serde_numpy import NumpyDeserializer
 
+PLOT_COLS = 3
 np.random.seed(33)
 
 
-def make_data(shape: Tuple[int, ...], dtypes: Sequence[Type]) -> bytes:
-    out = {}
-    for i, dtype in enumerate(dtypes):
-        if dtype in [int, np.int16, np.int32, np.int64]:
-            out[f"key_{i}"] = np.random.randint(-2**15, 2**15, size=shape).tolist()
-        elif dtype in [float, np.float16, np.float32, np.float64]:
-            out[f"key_{i}"] = (np.random.randn(np.prod(shape)).reshape(shape) * 2**8).tolist()
-        elif dtype in [bool, np.bool_]:
-            out[f"key_{i}"] = (np.random.rand(np.prod(shape)).reshape(shape) < 0.5).tolist()
-        elif dtype == str:
-            out[f"key_{i}"] = np.random.choice(list(string.ascii_letters), np.prod(shape), replace=True).reshape(shape).tolist()
-    return str.encode(json.dumps(out))
+def make_array(shape: Tuple[int, ...], dtype: Type) -> List[Any]:
+    if dtype in [int, np.int16, np.int32, np.int64]:
+        return np.random.randint(-2**15, 2**15, size=shape).tolist()
+    elif dtype in [np.uint16, np.uint32, np.uint64]:
+        return np.random.randint(0, 2**16, size=shape).tolist()
+    elif dtype in [float, np.float16, np.float32, np.float64]:
+        return (np.random.randn(np.prod(shape)).reshape(shape) * 2**8).tolist()
+    elif dtype in [bool, np.bool_]:
+        return (np.random.rand(np.prod(shape)).reshape(shape) < 0.5).tolist()
+    elif dtype == str:
+        return np.random.choice(list(string.ascii_letters), np.prod(shape), replace=True).reshape(shape).tolist()
+    else:
+        raise ValueError(f"dtype: {dtype} not recognised.")
 
 
-def json_numpy_loads(json_str: bytes, keys: Sequence, dtypes: Sequence[Type], load_str_func: Callable) -> Mapping[str, np.ndarray]:
-    out = load_str_func(json_str)
-    for i, key in enumerate(keys):
-        out[key] = np.array(out[key], dtypes[i])
+def make_data(shape: Tuple[int, ...], dtype: Type) -> bytes:
+    return str.encode(json.dumps(dict(key=make_array(shape, dtype))))
+
+
+def tranpose_lol(lol: List[List[Any]]) -> List[List[Any]]:
+    return [list(row) for row in zip(*lol)]
+
+
+def make_transposed_data(n_rows: int, dtypes: Sequence[Type]) -> bytes:
+    data = [ make_array((n_rows,), dtype) for dtype in dtypes]
+    data = tranpose_lol(data)
+    return str.encode(json.dumps(dict(key=data)))
+
+
+def orjson_then_numpy(json_str: bytes, dtype: Type) -> Mapping[str, np.ndarray]:
+    out = orjson.loads(json_str)
+    out["key"] = np.array(out["key"], dtype)
     return out
 
-def serde_numpy_loads(json_str: bytes, keys: Sequence[str], dtypes: Sequence[Type]) -> Mapping[str, List[np.ndarray]]:
+
+def orjson_then_numpy_tranpose(json_str: bytes, dtypes: Sequence[Type]) -> Mapping[str, np.ndarray]:
+    out = orjson.loads(json_str)
+    out["key"] = tranpose_lol(out["key"])
+    out["key"] = [np.array(out["key"][i], dtype) for i, dtype in enumerate(dtypes)]
+    return out
+
+
+def serde_numpy(json_str: bytes, deserialize: Callable) -> Mapping[str, List[np.ndarray]]:
     return deserialize(json_str, )
 
 
@@ -43,47 +68,111 @@ def _get_dtype(name: str) -> type:
         return getattr(np, name)
 
 
-def run_profile(n_rows: int, n_cols: int, dtypes: Sequence[Type] = (np.int32, np.float32), name: str = "", n_iters: int = 100):
-    data = make_data((n_rows, n_cols), dtypes)
-    keys = [f"key_{i}" for i in range(len(dtypes))]
-
-    times_json = []
-    for _ in range(n_iters):
-        time0 = time.time()
-        _ = json_numpy_loads(data, keys, dtypes, json.loads)
-        times_json.append(time.time() - time0)
-    
+def get_times_2d_array(n_rows: Sequence[int], n_cols: Sequence[int], dtype: Type, n_iters: int = 10) -> Mapping[str, np.ndarray]:
     times_orjson = []
-    
-    for _ in range(n_iters):
-        time0 = time.time()
-        _ = json_numpy_loads(data, keys, dtypes, orjson.loads)
-        times_orjson.append(time.time() - time0)
-    
     times_serde_numpy = []
-    deserializer = NumpyDeserializer.from_dict(dict(zip(keys, dtypes)))
-    for _ in range(n_iters):
-        time0 = time.time()
-        _ = deserializer.deserialize_json(data)
-        times_serde_numpy.append(time.time() - time0)
-    
-    print("-"*75)
-    print(f"{name} data times for json:: Mean: {np.mean(times_json):.3} Std: {np.std(times_json):.3}")
-    print(f"{name} data times for orjson:: Mean: {np.mean(times_orjson):.3} Std: {np.std(times_orjson):.3}")
-    print(f"{name} data times for serde numpy:: Mean: {np.mean(times_serde_numpy):.3} Std: {np.std(times_serde_numpy):.3}")
-    print(f"Speed up serde_numpy vs. orjson: {np.mean(times_orjson)/np.mean(times_serde_numpy):.3}x")
-    print(f"Speed up serde_numpy vs. json: {np.mean(times_json)/np.mean(times_serde_numpy):.3}x")
-    print("-"*75)
-    
+    n_rows_ = []
+    n_cols_ = []
+    deserializer = NumpyDeserializer.from_dict(dict(key=dtype))
+    for rows in n_rows:
+        for cols in n_cols:
+            data = make_data((rows, cols), dtype)
+            n_rows_.append(rows)
+            n_cols_.append(cols)
+            orjson_times_ = []
+            serde_np_times_ = []
+            for _ in range(n_iters):
+                
+                time0 = time.time()
+                _ = orjson_then_numpy(data, dtype)
+                orjson_times_.append(time.time() - time0)
+
+                time0 = time.time()
+                _ = deserializer.deserialize_json(data)
+                serde_np_times_.append(time.time() - time0)
+                
+            times_orjson.append(np.median(orjson_times_))
+            times_serde_numpy.append(np.median(serde_np_times_))
+
+    return dict(n_rows=np.array(n_rows_), 
+                n_cols=np.array(n_cols_), 
+                orjson_plus_numpy_time=np.array(times_orjson), 
+                serde_numpy_time=np.array(times_serde_numpy))
+
+
+def get_times_transposed_arrays(n_rows: Sequence[int], dtypes: Sequence[Type], n_iters: int = 10) -> Mapping[str, np.ndarray]:
+    times_orjson = []
+    times_serde_numpy = []
+    n_rows_ = []
+    deserializer = NumpyDeserializer.from_dict(dict(key=[dtypes]))
+    for rows in n_rows:
+        data = make_transposed_data(rows, dtypes)
+        n_rows_.append(rows)
+        orjson_times_ = []
+        serde_np_times_ = []
+        for _ in range(n_iters):
+            
+            time0 = time.time()
+            _ = orjson_then_numpy_tranpose(data, dtypes)
+            orjson_times_.append(time.time() - time0)
+
+            time0 = time.time()
+            _ = deserializer.deserialize_json(data)
+            serde_np_times_.append(time.time() - time0)
+            
+        times_orjson.append(np.median(orjson_times_))
+        times_serde_numpy.append(np.median(serde_np_times_))
+
+    return dict(n_rows=np.array(n_rows_),
+                orjson_plus_numpy_time=np.array(times_orjson), 
+                serde_numpy_time=np.array(times_serde_numpy))
+
+
+def plot_times_2d_array(times_rows: Mapping[str, np.ndarray], times_cols: Mapping[str, np.ndarray], ax: plt.Axes, title: str):
+    ax.plot(times_rows["n_rows"], times_rows["orjson_plus_numpy_time"] / times_rows["serde_numpy_time"], alpha=0.5)
+    ax.plot(times_cols["n_cols"], times_cols["orjson_plus_numpy_time"] / times_cols["serde_numpy_time"], alpha=0.5)
+    ax.grid()
+    ax.set_xscale("log")
+    ax.legend(["n_rows", "n_cols"])
+    ax.set_title(title)
+    ax.set_ylabel("speed up")
+
+
+def run_profile_2d_array(dtypes: Sequence[type]):
+    print("Profiling 2D array deserialization...")
+    n_rows = (2**np.arange(0, 20, 2)).astype(int)
+    n_cols = (2**np.arange(0, 20, 2)).astype(int)
+    plot_rows = int(np.ceil(len(dtypes) / PLOT_COLS))
+    fig = plt.figure(figsize=(6*PLOT_COLS, 4*plot_rows))
+    gs = gridspec.GridSpec(plot_rows, PLOT_COLS)
+    for i, dtype in enumerate(dtypes):
+        times_rows = get_times_2d_array(n_rows, [10], dtype)
+        times_cols = get_times_2d_array([10], n_cols, dtype)
+        ax = fig.add_subplot(gs[i // PLOT_COLS, i % PLOT_COLS])
+        plot_times_2d_array(times_rows, times_cols, ax, dtype.__name__)
+    plt.savefig("profile/2darr_profile.png")
+
+
+def run_profile_transposed_arrays(dtypes: Sequence[type]):
+    print("Profiling tranposed arrays deserialization...")
+    n_rows = (2**np.arange(0, 22, 2)).astype(int)
+    times = get_times_transposed_arrays(n_rows, dtypes)
+    fig = plt.figure(figsize=(6, 4))
+    plt.plot(times["n_rows"], times["orjson_plus_numpy_time"] / times["serde_numpy_time"], alpha=0.5)
+    plt.grid()
+    plt.xscale("log")
+    plt.legend(["n_rows"])
+    plt.title([dtype.__name__ for dtype in dtypes], fontsize=10)
+    plt.ylabel("speed up")
+    plt.savefig("profile/transpose_profile.png")
+
+        
 if __name__ == "__main__":
     import sys
-    dtypes = sys.argv[1:]
-    dtypes = [_get_dtype(name) for name in dtypes]
-    if len(dtypes) == 0:
-        raise ValueError("No dtypes specified!")
-    print(f"For dtypes: {dtypes}")
-    run_profile(2*1, 2, name="tiny", n_iters=1000, dtypes=dtypes)
-    run_profile(2**5, 2, name="small", n_iters=100, dtypes=dtypes)
-    run_profile(2**10, 4, name="medium", n_iters=10, dtypes=dtypes)
-    run_profile(2**15, 4, name="large", n_iters=5, dtypes=dtypes)
-    run_profile(2**20, 6, name="huge", n_iters=2, dtypes=dtypes)
+    dtypes = [_get_dtype(s) for s in sys.argv[1:]] or [np.int16, np.int32, np.int64, np.uint16, np.uint32, np.uint64, np.float32, np.float64, np.bool_]
+    run_profile_2d_array(dtypes)
+    run_profile_transposed_arrays(dtypes)
+    print("Done.")
+
+
+
